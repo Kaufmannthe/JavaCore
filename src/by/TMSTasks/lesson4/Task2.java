ef12595c716b68f9f3b74d5db8ececc911f1f547
Artyom Bohan <kaufmannthe@gmail.com>
2022-03-19 01:44:37 +0300

    Completed Task 1

--- src/by/TMSTasks/lesson4/Task2.java
+++ src/by/TMSTasks/lesson4/Task2.java
@@ -2,12 +2,14 @@ package by.TMSTasks.lesson4;
 
 public class Task2 {
     public static void main(String[] args) {
-        int hours = 24;
+        int hours = 21;
         int count = 0;
-        int ameba = 1;
-        for (int i = 0; i != hours/3;i++){
-          ameba = ameba + 2;
+        int ameba = 3;
+        for (int i = 0; i != hours/3;i++){ //По условию сказано, сколько их будет через 3,6,9 т.д. Соответственно каждые 3 часа
+
+          ameba = ameba * 2;
           count += 3;
+
         }
         System.out.println("Через " + count + " часа общая сумма амёб будет " + ameba + " штук");
     }
