e2af8b4065d871867f5920b0e7e1894ac0b30419
Artyom Bohan <kaufmannthe@gmail.com>
2022-03-19 01:20:15 +0300

    Completed Task 1

--- src/by/TMSTasks/lesson4/Task1.java
+++ src/by/TMSTasks/lesson4/Task1.java
@@ -2,12 +2,14 @@ package by.TMSTasks.lesson4;
 
 public class Task1 {
     public static void main(String[] args) {
-        float distance = 10;
-        int days = 7;
+        int distance = 10;
+        int days = 6;
+        int count = 10;
         for(int i = 0; i <= days; i++){
-            distance = distance + (distance/10);
+            distance = distance + distance/10;
+            count = count + distance;
             distance = Math.round(distance);
     }
-        System.out.println(distance + " киллометров пробежит спортсмен");
+        System.out.println(count + " киллометров пробежит спортсмен");
     }
 }
