b4a67feac18a5a7fa6ac86308a8ea057bc01060c
Artyom Bohan <kaufmannthe@gmail.com>
2022-03-21 00:07:20 +0300

    Completed Task 3

--- src/by/TMSTasks/lesson4/Task3.java
+++ src/by/TMSTasks/lesson4/Task3.java
@@ -1,20 +1,13 @@
 package by.TMSTasks.lesson4;
 
-import java.util.Scanner;
-
 public class Task3 {
     public static void main(String[] args) {
-        int a, b;
-        int result = 0;
-        Scanner x = new Scanner(System.in);
-        System.out.print("Введите первое число: ");
-        a = x.nextInt();
-        System.out.print("Введите второе число: ");
-        b = x.nextInt();
-        for (int count = 0; count != b; count++){
-            result = result + (a + b)/2;
-
-        }
-        System.out.println("Значение умножения является: " + result);
+    int summ = 0;
+    int count = 1;
+    while (count <=256){
+        summ = summ + count;
+        count = count + count;
+    }
+        System.out.println("Сумма чисел равна: " + summ);
     }
 }
