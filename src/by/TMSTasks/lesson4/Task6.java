54ad2db04afa6a009470e723d7acecfa41915205
Artyom Bohan <kaufmannthe@gmail.com>
2022-03-21 01:28:45 +0300

    Completed Task 6

---
+++ src/by/TMSTasks/lesson4/Task6.java
@@ -0,0 +1,11 @@
+package by.TMSTasks.lesson4;
+
+public class Task6 {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count != 100){
+            count = count + 2;
+            System.out.println(count);
+        }
+    }
+}
