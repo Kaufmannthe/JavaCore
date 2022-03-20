81a2773b57ba09d3df851b7153020b2c0d47f8ed
Artyom Bohan <kaufmannthe@gmail.com>
2022-03-21 00:58:44 +0300

    Completed Task 4

---
+++ src/by/TMSTasks/lesson4/Task4.java
@@ -0,0 +1,18 @@
+package by.TMSTasks.lesson4;
+
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        System.out.print("Пожалуйста, введите первый множитель: ");
+        int a = x.nextInt();
+        System.out.print("Введите второй множитель: ");
+        int b = x.nextInt();
+        int summ = 0; int result = 0; int count = 0;
+        for (int i = 0; i != b; i++){
+            result = result + a;
+        }
+        System.out.println("Значение умножения равно: " + result);
+        }
+    }
