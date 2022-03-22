d7f35d7bddfd7eaccba54cdff7a6a045aa5b2cb3
Artyom Bohan <kaufmannthe@gmail.com>
2022-03-22 21:51:55 +0300

    Completed Task 4

--- src/by/TMSTasks/lesson5_Arrays/Task4.java
+++ src/by/TMSTasks/lesson5_Arrays/Task4.java
@@ -6,14 +6,18 @@ public class Task4 {
     public static void main(String[] args) {
         int[] array = new int[21];
         Random random = new Random();
-        System.out.println("Значения массива:   ");
+        System.out.println("Значения массива:");
         for (int i = 0; i <= 20; i++) {
-            array[i] = random.nextInt(1,100);
-            System.out.print(array[i]);
-            if (array[i] % 2 == 1){
+            array[i] = random.nextInt(1, 100);
+            if (i % 2 == 1){
+                array[i] = 0;
+                System.out.print(array[i] + " ");
+            }else {
                 System.out.print(array[i] + " ");
             }
 
         }
+
     }
+
 }
