67a30009e154e2b3b5cbd580be5e86c8801c81a9
Artyom Bohan <kaufmannthe@gmail.com>
2022-03-31 16:41:00 +0300

    Added OOP Tasks

--- src/by/TMSTasks/lesson6_Objects/Task8/Shape.java
+++ src/by/TMSTasks/lesson6_Objects/Task8/Shape.java
@@ -3,5 +3,17 @@ package by.TMSTasks.lesson6_Objects.Task8;
 public abstract class Shape {
     private String color;
 
+    public Shape(String color){
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
     abstract void draw();
 }
