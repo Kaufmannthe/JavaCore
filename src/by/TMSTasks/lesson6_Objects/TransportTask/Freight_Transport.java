ccc54db186817a06ff2251f3e17781116a2236b1
Artyom Bohan <kaufmannthe@gmail.com>
2022-04-07 00:35:56 +0300

    Added OOP Tasks

---
+++ src/by/TMSTasks/lesson6_Objects/TransportTask/Freight_Transport.java
@@ -0,0 +1,33 @@
+package by.TMSTasks.lesson6_Objects.TransportTask;
+
+public class Freight_Transport extends Ground_Transport{
+    private int loadCapacity;
+
+    public int getLoadCapacity() {
+        return loadCapacity;
+    }
+
+    public void setLoadCapacity(int loadCapacity) {
+        this.loadCapacity = loadCapacity;
+    }
+
+    public Freight_Transport(int enginePower, int maxSpeed, int weight, String brand, int wheels, int fuelConsumption, int loadCapacity) {
+        super(enginePower, maxSpeed, weight, brand, wheels, fuelConsumption);
+        this.loadCapacity = loadCapacity;
+    }
+
+    public void transportInfo(){
+        System.out.println("Мощность легковой машины: " + getEnginePower() + " л/c, максимальная скорость: " + getMaxSpeed() + "км/ч, " +
+                "масса машины: " + getWeight() + " т., марка машины: " + getBrand() + ", количество колёс: " + getWheels() +
+                ", расход топлива: " + getFuelConsumption() + ", грузоподъёмность машины: " + getLoadCapacity() + " т.," + "мощность машины в килловатах: " +
+                 getEnginePower()*0.74 + "кВ");
+    }
+
+    public void loadCapacityCheck(int weight){
+        if (weight > loadCapacity){
+            System.out.println("Вам необходим грузовик побольше.");
+        }else {
+            System.out.println("Грузовик загружен.");
+        }
+    }
+}
---
+++ src/by/TMSTasks/lesson6_Objects/TransportTask/TransportAPP.java
@@ -0,0 +1,41 @@
+package by.TMSTasks.lesson6_Objects.TransportTask;
+
+public class TransportAPP {
+    public static void main(String[] args) {
+        Ground_Transport passengerCar = new Passenger_Car(150,200, 3, "BMW X3",4,
+                5,"в ристайлинге",4);
+
+        Passenger_Car passenger_car1 = new Passenger_Car(150,200,3,"BMW X3",4
+        ,5,"в ристайлинге",4);
+
+        Freight_Transport freightTransport = new Freight_Transport(200,70,6,"MAZ",
+                4,14,20);
+
+        Civil_Air_Transport civilAirTransport = new Civil_Air_Transport(300,800,10,
+                "Boing",50,600,100,true);
+
+        Military_Air_Transport militaryAirTransport = new Military_Air_Transport(500, 900,6,
+                "MX10",40,500,false,10);
+
+        Ground_Transport ground_transport [] = {passengerCar};
+        for(Ground_Transport ground_transport1 : ground_transport){     //test transportInfo
+            ground_transport1.transportInfo();
+        }
+
+        System.out.println();
+        passenger_car1.fuelScore(150); //test fuelScore
+
+        System.out.println();
+        freightTransport.loadCapacityCheck(31);     //test loadCapacityCheck
+
+
+        System.out.println();
+        civilAirTransport.passengerValueCheck(101);         //test passengerValueCheck
+
+        System.out.println();
+        militaryAirTransport.rocketValueCheck();            //test rocketValueCheck
+
+        System.out.println();
+        militaryAirTransport.catapultSystemCheck();         //test catapultSystemCheck
+    }
+}
