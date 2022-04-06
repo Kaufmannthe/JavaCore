ca75f081feea791d79619a1d918e4218e295f565
Artyom Bohan <kaufmannthe@gmail.com>
2022-04-06 22:04:46 +0300

    Added OOP Tasks

---
+++ src/by/TMSTasks/lesson6_Objects/Task9_OnlineStore/Basket.java
@@ -0,0 +1,13 @@
+package by.TMSTasks.lesson6_Objects.Task9_OnlineStore;
+
+public class Basket {
+    private int[] basketCount;
+
+    public int[] getBasketCount() {
+        return basketCount;
+    }
+
+    public void setBasketCount(int[] basketCount) {
+        this.basketCount = basketCount;
+    }
+}
---
+++ src/by/TMSTasks/lesson6_Objects/Task9_OnlineStore/Category.java
@@ -0,0 +1,20 @@
+package by.TMSTasks.lesson6_Objects.Task9_OnlineStore;
+
+public class Category {
+    private String categoryName;
+    private int[] product;
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public int[] getProduct() {
+        return product;
+    }
+
+    public Category(String categoryName, int[] product) {
+        this.categoryName = categoryName;
+        this.product = product;
+    }
+}
+
