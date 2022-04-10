9d91be7efa8da1f2b0f38e4e76950d959214ee71
Artyom Bohan <kaufmannthe@gmail.com>
2022-04-10 20:10:35 +0300

    Added OOP Tasks

---
+++ src/by/TMSTasks/lesson6_Objects/Task2_Interface_Instrument/Drums.java
@@ -0,0 +1,18 @@
+package by.TMSTasks.lesson6_Objects.Task2_Interface_Instrument;
+
+public class Drums implements Instrument{
+    private int drumsSize;
+
+    public Drums(int drumsSize) {
+        this.drumsSize = drumsSize;
+    }
+
+    public int getDrumsSize() {
+        return drumsSize;
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Играет барабан," + getDrumsSize() + " см.");
+    }
+}
