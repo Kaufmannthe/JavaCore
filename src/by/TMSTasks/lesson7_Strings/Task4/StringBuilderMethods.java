818a22f73f66ff4fd269980412fed4b8f424be28
Artyom Bohan <kaufmannthe@gmail.com>
2022-04-22 03:26:41 +0300

    Added String Task

--- src/by/TMSTasks/lesson7_Strings/Task4/StringBuilderMethods.java
+++ src/by/TMSTasks/lesson7_Strings/Task4/StringBuilderMethods.java
@@ -29,7 +29,9 @@ public class StringBuilderMethods {
         StringBuilder sum = new StringBuilder("Сумма чисел ").append(this.a).append(" и ").append(this.b).append(
                 " = ").append(this.a + this.b).append("\n");
         int equalsSingDeleting = sum.indexOf("="); // поиск индекса символа "=".
+
         StringBuilder sumAfterDeleting = sum.deleteCharAt(equalsSingDeleting); // удаление этого символа.
+
         StringBuilder sumAfterInsert = sumAfterDeleting.insert(equalsSingDeleting,"равно"); //добавление слова "равно" по месту нахождения символа "="
         System.out.print(sumAfterInsert); //Task 2
     }
@@ -37,7 +39,9 @@ public class StringBuilderMethods {
     public void decreaseOfIntegers(){
         StringBuilder decrease = new StringBuilder("Разница чисел ").append(this.a).append(" и ").append(this.b).append(
                 " = ").append(a - b);
+
         int equalsSingDeleting = decrease.indexOf("="); //поиск индекса символа "=".
+
         System.out.println(decrease.replace(equalsSingDeleting,equalsSingDeleting + 1,"равно")); //Task 3
        /*Замена символа "=" на слово "равно" путём указания в сигнатуре того индекса, по которому определён вход символа, тот же индекс + 1
                 и указания слова "равно".*/
