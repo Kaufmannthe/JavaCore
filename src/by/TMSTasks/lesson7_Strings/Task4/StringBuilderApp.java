bd5e5feedac3b09cf7d6e5813b315cd26144f4dc
Artyom Bohan <kaufmannthe@gmail.com>
2022-04-22 03:25:54 +0300

    Added String Task

---
+++ src/by/TMSTasks/lesson7_Strings/Task4/StringBuilderApp.java
@@ -0,0 +1,11 @@
+package by.TMSTasks.lesson7_Strings.Task4;
+
+public class StringBuilderApp {
+    public static void main(String[] args) {
+    StringBuilderMethods firstSum = new StringBuilderMethods(3,56);
+
+    firstSum.sumOfIntegers();
+    firstSum.decreaseOfIntegers();
+    firstSum.multiplicationOfIntegers();
+    }
+}
