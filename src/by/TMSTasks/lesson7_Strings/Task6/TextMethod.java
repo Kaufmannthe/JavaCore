13bc414dab87b910d8cb097427ac011f2c43ed70
Artyom Bohan <kaufmannthe@gmail.com>
2022-04-23 02:04:07 +0300

    Added String Task

---
+++ src/by/TMSTasks/lesson7_Strings/Task6/TextMethod.java
@@ -0,0 +1,27 @@
+package by.TMSTasks.lesson7_Strings.Task6;
+
+public class TextMethod {
+    private StringBuilder stringBuilder;
+
+    public StringBuilder getStringBuilder() {
+        return stringBuilder;
+    }
+
+    public void setStringBuilder(StringBuilder stringBuilder) {
+        this.stringBuilder = stringBuilder;
+    }
+
+    public TextMethod() {
+    }
+
+    public TextMethod(StringBuilder stringBuilder) {
+        this.stringBuilder = stringBuilder;
+    }
+    public void findAMiddle(){
+        int stringLength = this.stringBuilder.length();
+        int theMiddleOfString = stringLength / 2;
+        StringBuilder result = new StringBuilder("Середина строки: ").append(this.stringBuilder.charAt(theMiddleOfString)).append(
+                this.stringBuilder.charAt(theMiddleOfString + 1));
+        System.out.println(result);
+    }
+}
