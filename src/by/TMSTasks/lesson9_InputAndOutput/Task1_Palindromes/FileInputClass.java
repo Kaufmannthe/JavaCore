5b5e93235e543277a43d9acae6ba0fd1110885c8
Artyom Bohan <kaufmannthe@gmail.com>
2022-04-26 01:56:16 +0300

    Added Input and Output Task

--- src/by/TMSTasks/lesson9_InputAndOutput/Task1_Palindromes/FileInputClass.java
+++ src/by/TMSTasks/lesson9_InputAndOutput/Task1_Palindromes/FileInputClass.java
@@ -15,15 +15,20 @@ public class FileInputClass {
         try (BufferedReader reader = new BufferedReader(new FileReader((input)))) {
             String line;
 
-            while ((line = reader.readLine()) != null) {
-                StringBuilder reversedLine = new StringBuilder(line);
-                reversedLine.reverse();
+            while ((line = reader.readLine()) != null) {        //Поиск слов в файле Text.txt
+                StringBuilder reversedLine = new StringBuilder(line);    //Создание SB для риверса строки line.txt.
+                reversedLine.reverse();                                 //Переворачиваем проверяемую строку.
                 if (line.equals(reversedLine.toString())) {
-                    System.out.println(line);
+
+                    //Если перевернётая строка == обычной строке, то создаётся файл replacedText и туда вносятся похожие слова.
+
+                    System.out.println(line);             //Проверка вывода.
 
                     try (BufferedWriter writer = new BufferedWriter(new FileWriter("" +
                             "C:\\JavaCoreTMS\\src\\by\\TMSTasks\\lesson9_InputAndOutput\\Task1\\replacedText.txt", true))) {
-                        writer.write(line + "\n");
+                        //Указание директории, в которой необходимо создать файл и указать слова палиндромы.
+
+                        writer.write(line + "\n"); //Запись палиндромов на файл replacedText.txt
 
 
                     } catch (IOException e) {
