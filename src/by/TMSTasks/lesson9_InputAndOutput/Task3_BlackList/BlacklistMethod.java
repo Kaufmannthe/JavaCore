03274dc56f9319ec6a28629cbf6294d58a66f1fb
Artyom Bohan <kaufmannthe@gmail.com>
2022-04-30 18:44:07 +0300

    Added Input and Output Task 3

---
+++ src/by/TMSTasks/lesson9_InputAndOutput/Task3_BlackList/BlacklistMethod.java
@@ -0,0 +1,43 @@
+package by.TMSTasks.lesson9_InputAndOutput.Task3_BlackList;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Objects;
+
+public class BlacklistMethod {
+    private int count;
+
+    public BlacklistMethod() {
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int blackListCheck(String string, String stringLine) throws IOException {
+        String input = "C:\\JavaCoreTMS\\src\\by\\TMSTasks\\lesson9_InputAndOutput\\Task3_BlackList\\blacklist.txt";
+        String blackLine;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(input))) {
+
+            while ((blackLine = reader.readLine()) != null) {
+                if (Objects.equals(string, blackLine)) {
+                    setCount(-1);
+                    StringBuilder failure = new StringBuilder("Найдено запрещённое слово: ").append(blackLine)
+                            .append(". \nВ строке: ").append(stringLine);
+                    throw new BlackListExceptions(String.valueOf(failure));
+                }
+            }
+        } catch (BlackListExceptions e) {
+            System.err.println(e.getMessage());
+
+        }
+        return getCount();
+    }
+}
+
