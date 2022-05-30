f81520f6e5a48fc36e2e113d8b34473cf184d328
Artyom <kaufmannthe@gmail.com>
2022-05-30 03:59:41 +0300

    Add synchronized thread task with outputting arrays of integers

---
+++ src/by/tmstasks/multithreading/save_as_thread/Main.java
@@ -0,0 +1,30 @@
+package by.tmstasks.multithreading.save_as_thread;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Random random = new Random();
+
+        ArrayList<Integer> arrayList1 = new ArrayList<>();
+        ArrayList<Integer> arrayList2 = new ArrayList<>();
+        ArrayList<Integer> arrayList3 = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            arrayList1.add(random.nextInt(0, 10));
+            arrayList2.add(random.nextInt(0, 10));
+            arrayList3.add(random.nextInt(0, 10));
+        }
+
+        Thread thread1 = new Thread(new SaveAsThread(arrayList1), "Thread 1");
+        Thread thread2 = new Thread(new SaveAsThread(arrayList2), "Thread 2");
+        Thread thread3 = new Thread(new SaveAsThread(arrayList3), "Thread 3");
+
+        thread1.start();
+        thread1.join();
+        thread2.start();
+        thread2.join();
+        thread3.start();
+    }
+}
---
+++ src/by/tmstasks/multithreading/save_as_thread/SaveAsThread.java
@@ -0,0 +1,52 @@
+package by.tmstasks.multithreading.save_as_thread;
+
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class SaveAsThread implements Runnable {
+
+    private ArrayList<Integer> list;
+
+    public ArrayList<Integer> getList() {
+        return list;
+    }
+
+    public void setList(ArrayList<Integer> list) {
+        this.list = list;
+    }
+
+    public SaveAsThread(ArrayList<Integer> list) {
+        this.list = list;
+    }
+
+    @Override
+    public void run() {
+        String output = "src/by/tmstasks/multithreading/save_as_thread/result.txt";
+        System.out.print(Thread.currentThread().getName() + ": ");
+        synchronized (this) {
+            try {
+                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(output, true));
+
+                bufferedWriter.write(Thread.currentThread().getName() + ": ");
+                for (Integer integer : this.list) {
+                    bufferedWriter.write(integer.toString());
+                    System.out.print(integer);
+                }
+
+                bufferedWriter.write("\n");
+                bufferedWriter.close();
+                System.out.println();
+
+            } catch (IOException e) {
+                System.err.println("Error");
+            }
+        }
+
+    }
+}
+
+
+
